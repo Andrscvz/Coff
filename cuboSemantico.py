2be5b787729654979d55008d7454fc292822a0a6
andrscvz <andrscvz@gmail.com>
2015-10-31 19:05:25 -0600

    Cubo

---
+++ cuboSemantico.py
@@ -0,0 +1,236 @@
+class cuboSemantico:
+	
+	cubo_semantico = None
+
+	def __init__(self):
+		self.cubo_semantico = {
+		'=': {
+			'ENTERO': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': None,
+				'TEXTO': None
+			},
+			'DECIMAL': {
+				'ENTERO': None,
+				'DECIMAL': 'DECIMAL',
+				'TEXTO': None
+			},
+			'TEXTO': {
+				'ENTERO': None,
+				'DECIMAL': None,
+				'TEXTO': 'TEXTO'
+			}
+		},
+		'&&': {
+			'ENTERO': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': None,
+				'TEXTO': None,
+			},
+			'DECIMAL': {
+				'ENTERO': None,
+				'DECIMAL': None,
+				'TEXTO': None
+			},
+			'TEXTO': {
+				'ENTERO': None,
+				'DECIMAL': None,
+				'TEXTO': None
+			}
+		},
+		'||': {
+			'ENTERO': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': None,
+				'TEXTO': None
+			},
+			'DECIMAL': {
+				'ENTERO': None,
+				'DECIMAL': None,
+				'TEXTO': None
+			},
+			'TEXTO': {
+				'ENTERO': None,
+				'DECIMAL': None,
+				'TEXTO': None
+			}
+		},
+		'>': {
+			'ENTERO': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': 'ENTERO',
+				'TEXTO': None
+			},
+			'DECIMAL': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': 'ENTERO',
+				'TEXTO': None
+			},
+			'TEXTO': {
+				'ENTERO': None,
+				'DECIMAL': None,
+				'TEXTO': 'ENTERO'
+			}
+		},
+		'<': {
+			'ENTERO': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': 'ENTERO',
+				'TEXTO': None
+			},
+			'DECIMAL': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': 'ENTERO',
+				'TEXTO': None
+			},
+			'TEXTO': {
+				'ENTERO': None,
+				'DECIMAL': None,
+				'TEXTO': 'ENTERO'
+			}
+		},
+		'!=': {
+			'ENTERO': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': 'ENTERO',
+				'TEXTO': None
+			},
+			'DECIMAL': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': 'ENTERO',
+				'TEXTO': None
+			},
+			'TEXTO': {
+				'ENTERO': None,
+				'DECIMAL': None,
+				'TEXTO': 'ENTERO'
+			}
+		},
+		'>=': {
+			'ENTERO': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': 'ENTERO',
+				'TEXTO': None
+			},
+			'DECIMAL': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': 'ENTERO',
+				'TEXTO': None
+			},
+			'TEXTO': {
+				'ENTERO': None,
+				'DECIMAL': None,
+				'TEXTO': 'ENTERO'
+			}
+		},
+		'<=': {
+			'ENTERO': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': 'ENTERO',
+				'TEXTO': None
+			},
+			'DECIMAL': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': 'ENTERO',
+				'TEXTO': None
+			},
+			'TEXTO': {
+				'ENTERO': None,
+				'DECIMAL': None,
+				'TEXTO': 'ENTERO'
+			}
+		},
+		'==': {
+			'ENTERO': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': 'ENTERO',
+				'TEXTO': None
+			},
+			'DECIMAL': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': 'ENTERO',
+				'TEXTO': None
+			},
+			'TEXTO': {
+				'ENTERO': None,
+				'DECIMAL': None,
+				'TEXTO': 'ENTERO'
+			}
+		},
+		'+': {
+			'ENTERO': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': 'DECIMAL',
+				'TEXTO': None
+			},
+			'DECIMAL': {
+				'ENTERO': 'DECIMAL',
+				'DECIMAL': 'DECIMAL',
+				'TEXTO': None
+			},
+			'TEXTO': {
+				'ENTERO': None,
+				'DECIMAL': None,
+				'TEXTO': 'TEXTO'
+			}
+		},
+		'-': {
+			'ENTERO': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': 'DECIMAL',
+				'TEXTO': None
+			},
+			'DECIMAL': {
+				'ENTERO': 'DECIMAL',
+				'DECIMAL': 'DECIMAL',
+				'TEXTO': None
+			},
+			'TEXTO': {
+				'ENTERO': None,
+				'DECIMAL': None,
+				'TEXTO': None
+			}
+		},
+		'*': {
+			'ENTERO': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': 'DECIMAL',
+				'TEXTO': None
+			},
+			'DECIMAL': {
+				'ENTERO': 'DECIMAL',
+				'DECIMAL': 'DECIMAL',
+				'TEXTO': None
+			},
+			'TEXTO': {
+				'ENTERO': None,
+				'DECIMAL': None,
+				'TEXTO': None
+			}
+		},
+		'/': {
+			'ENTERO': {
+				'ENTERO': 'ENTERO',
+				'DECIMAL': 'DECIMAL',
+				'TEXTO': None
+			},
+			'DECIMAL': {
+				'ENTERO': 'DECIMAL',
+				'DECIMAL': 'DECIMAL',
+				'TEXTO': None
+			},
+			'TEXTO': {
+				'ENTERO': None,
+				'DECIMAL': None,
+				'TEXTO': None
+			}
+		}
+	}
+
+	def checarSemanticaExp(self,oper1,oper2,op):
+		res = self.cubo_semantico.get(op)
+		if res != None:
+			res = res.get(oper1)
+			if res != None:
+				res = res.get(oper2)
+		return res
